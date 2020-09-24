ea4d20879c531a26d86de140e330e9e441bea2fe
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-24 18:13:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
 require 'open-uri'
-doc = Nokogiri::HTML(html)
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".headline-26OIBN")
 html = open("https://flatironschool.com/")
